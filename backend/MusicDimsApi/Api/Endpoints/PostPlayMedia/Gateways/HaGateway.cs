1e04eeaa9f1addb8a948716e97fea74f3a8adec2
HBDK <1486019+HBDK@users.noreply.github.com>
2025-10-11 23:09:42 +0200

    chop hop

--- backend/MusicDimsApi/Api/Endpoints/PostPlayMedia/Gateways/HaGateway.cs
+++ backend/MusicDimsApi/Api/Endpoints/PostPlayMedia/Gateways/HaGateway.cs
@@ -10,16 +10,36 @@ public class HaGateway(IHttpClientFactory clientFactory, ILogger<HaGateway> logg
 
     public async Task PlayMedia(string playerId, string mediaId, string mediaType)
     {
-        var body = new PostPlayMediaBody($"media_player.{playerId}", mediaType, mediaId, "replace");
+        var body = new PostPlayMediaBody(
+            $"media_player.{playerId}",
+            mediaType,
+            mediaId,
+            "replace");
+
         var client = clientFactory.CreateClient(HomeAssistantClient.Name);
-        var response = await client.PostAsync(PostPlayMediaPath, new StringContent(JsonSerializer.Serialize(body), System.Text.Encoding.UTF8, "application/json"));
+
+        var response = await client.PostAsync(
+            PostPlayMediaPath,
+            new StringContent(
+                JsonSerializer.Serialize(body),
+                System.Text.Encoding.UTF8,
+                "application/json"));
 
         var test = await response.Content.ReadAsStringAsync();
         response.EnsureSuccessStatusCode();
     }
 }
 
-public record PostPlayMediaBody([property: JsonPropertyName("entity_id")]string EntityId, [property: JsonPropertyName("media_type")]string MediaType, [property: JsonPropertyName("media_id")] string MediaId, [property: JsonPropertyName("enqueue")] string Enqueue);
+public record PostPlayMediaBody(
+    [property: JsonPropertyName("entity_id")]
+    string EntityId,
+    [property: JsonPropertyName("media_type")]
+    string MediaType,
+    [property: JsonPropertyName("media_id")]
+    string MediaId,
+    [property: JsonPropertyName("enqueue")]
+    string Enqueue);
+
 public interface IHaGateway
 {
     Task PlayMedia(string playerId, string mediaId, string mediaType);
