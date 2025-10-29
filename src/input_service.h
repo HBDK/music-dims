3a64f0107cd33b7aa7d7ec37afccb48523601282
HBDK <1486019+HBDK@users.noreply.github.com>
2025-10-29 21:13:31 +0100

    make it work with the 3.5

--- src/input_service.h
+++ src/input_service.h
@@ -5,7 +5,7 @@
 class InputService {
 public:
     static constexpr uint8_t PIN_ENC_A    = 22;
-    static constexpr uint8_t PIN_ENC_B    = 27;
+    static constexpr uint8_t PIN_ENC_B    = 35;
     static constexpr uint8_t PIN_ENC_SW   = 1;
     static constexpr uint8_t PIN_BACK_BTN = 3;
     static constexpr uint8_t IMPULSES_PER_CLICK = 1;
